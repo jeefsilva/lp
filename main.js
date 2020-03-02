ae776acf488749289495560974ac13d6199187f5
Jefferson Silva <jbds.web@gmail.com>
2020-03-02 00:10:35 -0300

    Adicionando Efeito de Full Screen Photo

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+
+
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+
 function zoomImg(classImg) {
-    let element = document.getElementById(classImg);
-    element.classList.toggle("zoom-img");
-    imgZoom = true;
+    var imgRender = document.getElementById(classImg);
+    modal.style.display = "block";
+    modalImg.src = imgRender.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
