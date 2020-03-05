d8b1572521a5065e940bf8e5025359c5e9b31473
jbds.web@gmail.com <jefferson.silva@voxus.tv>
2020-03-05 13:52:06 -0300

    Adicionando script dos depoimentos.

--- main.js
+++ main.js
@@ -24,3 +24,44 @@ function dropDown() {
     dropDownId.classList.toggle('closed')
     dropClose.classList.toggle('hide')
 }
+
+var logoAmaro = document.getElementById("logo-amaro")
+var fieldAmaro = document.getElementById("amaro-field")
+var logoSage = document.getElementById("logo-sage")
+var fieldSage = document.getElementById("sage-field")
+var logoOgilvy = document.getElementById("logo-ogilvy")
+var fieldOgilvy = document.getElementById("ogilvy-field")
+
+function logoAmaroEnable() {
+  logoAmaro.classList.add("logo-amaro-selected");
+  logoAmaro.classList.remove("logo-amaro");
+  logoSage.classList.add("logo-sage-deselected");
+  logoOgilvy.classList.add("logo-ogilvy");
+  logoOgilvy.classList.remove("logo-ogilvy-selected");
+  fieldOgilvy.classList.add("display-none");
+  fieldAmaro.classList.remove("display-none");
+  fieldSage.classList.add("display-none");
+
+}
+
+function logoOgilvyEnable() {
+  logoOgilvy.classList.add("logo-ogilvy-selected");
+  logoOgilvy.classList.remove("logo-ogilvy");
+  logoSage.classList.add("logo-sage-deselected");
+  logoAmaro.classList.add("logo-amaro");
+  logoAmaro.classList.remove("logo-amaro-selected");
+  fieldOgilvy.classList.remove("display-none");
+  fieldAmaro.classList.add("display-none");
+  fieldSage.classList.add("display-none");
+}
+
+function logoSageEnable() {
+  logoOgilvy.classList.remove("logo-ogilvy-selected");
+  logoOgilvy.classList.add("logo-ogilvy");
+  logoSage.classList.remove("logo-sage-deselected");
+  logoAmaro.classList.add("logo-amaro");
+  logoAmaro.classList.remove("logo-amaro-selected");
+  fieldOgilvy.classList.add("display-none");
+  fieldAmaro.classList.add("display-none");
+  fieldSage.classList.remove("display-none");
+}
